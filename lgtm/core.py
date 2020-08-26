931711794a768c822689f45796131988d4d3705b
kopei-matsu <mkfatcat13@gmail.com>
2020-08-26 18:15:23 +0900

    setup.pyを作成

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,4 +1,6 @@
 import click
+from lgtm.drawer import save_with_message
+from lgtm.image_source import get_image
 
 @click.command()
 @click.option('--message', '-m', default='LGTM', show_default=True,
@@ -11,6 +13,6 @@ def cli(keyword, message):
 
 
 def lgtm(keyword, message):
-    # todo:ここにロジックを追加していく
-    pass
+    with get_image(keyword) as fp:
+        save_with_message(fp, message)
 
